928ef29e11d30608a4682c4719d113d36b71ee38
ThuTrang21 <thut15794@gmail.com>
2025-01-16 16:28:58 +0700

    update

--- showList/src/App.tsx
+++ showList/src/App.tsx
@@ -1,35 +1,152 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Button, Modal, Space, Table, TableProps } from "antd";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Input } from "antd";
+import { Field, Form, Formik } from "formik";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface DataType {
+  id: number;
+  name?: string;
+  price?: number;
+  image?: string;
 }
 
-export default App
+const products: DataType[] = [
+  {
+    id: 1,
+    name: "Bánh Kem Dâu",
+    price: 150000,
+    image:
+      "https://cdn.tgdd.vn/2020/12/CookRecipe/GalleryStep/thanh-pham-1090.jpg",
+  },
+  {
+    id: 2,
+    name: "Bánh Mousse Socola",
+    price: 200000,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPF4PnCVhNNCKu7VEv6UFMS4hAZFiM0ph0kw&s",
+  },
+  {
+    id: 3,
+    name: "Bánh Tiramisu",
+    price: 180000,
+    image:
+      "https://product.hstatic.net/200000411281/product/3_c31e16c02c9d4afab72bb20e13abe84d_master.png",
+  },
+];
+
+const columns: TableProps<DataType>["columns"] = [
+  {
+    title: "Hình ảnh",
+    dataIndex: "image",
+    key: "image",
+    render: (text: string) => (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <img src={text} alt="product" style={{ width: 50, height: 50 }} />
+      </div>
+    ),
+    align: "center",
+  },
+  {
+    title: "Tên bánh",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Giá",
+    dataIndex: "price",
+    key: "price",
+  },
+  {
+    title: "Hành động",
+    key: "action",
+    render: () => (
+      <Space size="middle">
+        <a>Sửa</a>
+        <a>Xóa</a>
+      </Space>
+    ),
+  },
+];
+function App() {
+  const [open, setOpen] = useState(false);
+  const [list, setList] = useState(products);
+
+  const showModal = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleReset = (resetForm:any) => {
+resetForm();
+  };
+
+  return (
+    <div className="w-full py-10">
+      <div className="flex justify-evenly items-center mb-4">
+        <h1 className="text-4xl font-bold py-2">Danh sách sản phẩm</h1>
+        <Button type="primary" onClick={showModal}>
+          Thêm
+        </Button>
+      </div>
+      <Table<DataType> columns={columns} dataSource={list} />
+      <Modal
+        open={open}
+        title="Thêm sản phẩm"
+        onCancel={handleClose}
+       footer={null}
+      >
+      <Formik
+          initialValues={{ name: "", price: "", image: "" }}
+
+          onSubmit={(values, { resetForm }) => {
+            setList([...list, { ...values, id: list.length + 1, price: Number(values.price) }]);
+            resetForm();
+            setOpen(false);
+          }}
+        >
+        
+           {({resetForm})=> <Form>
+              <div className="mb-4">
+                <label>Hình ảnh</label>
+                <Field name="image">
+                  {({ field }: any) => <Input {...field} />}
+                </Field>
+              </div>
+
+              <div className="mb-4">
+                <label>Tên sản phẩm</label>
+                <Field name="name">
+                  {({ field }: any) => <Input {...field} />}
+                </Field>
+              </div>
+
+              <div className="mb-4">
+                <label>Giá</label>
+                <Field name="price">
+                  {({ field }: any) => <Input {...field} type="number" />}
+                </Field>
+              </div>
+              <div className="flex justify-end gap-2">
+                <Button type="default" onClick={()=>handleReset(resetForm)}>Hoàn tác</Button>
+                <Button type="primary" htmlType="submit">Thêm</Button>
+              </div>
+            </Form>}
+
+        </Formik>
+      </Modal>
+    </div>
+  );
+}
+
+export default App;
